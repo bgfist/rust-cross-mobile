80db393b2ad2847d899e0c416ddebb59b0cafb40
周旺 <zhouwang@mucang.cn>
2024-08-07 19:01:54 +0800

    feat: 优化打包体积

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -4,5 +4,8 @@ arch=aarch64
 target=aarch64-linux-android
 outdir=./ports/android
 
+# release模式下把符号都strip掉了，没法生成胶水代码，这里用debug模式生成吧
+cargo ndk -t arm64-v8a -o $outdir/jniLibs build
+cargo run --bin uniffi_bindgen generate --library ./target/$target/debug/libjk_core_question.so --language kotlin --out-dir $outdir
+
 cargo ndk -t arm64-v8a -o $outdir/jniLibs build --release
-cargo run --bin uniffi_bindgen generate --library ./target/$target/release/libjk_core_question.so --language kotlin --out-dir $outdir
--- scripts/build-ios.sh
+++ scripts/build-ios.sh
@@ -4,6 +4,12 @@ arch=aarch64
 target=${arch}-apple-ios
 outdir=./ports/ios
 
+# release模式下把符号都strip掉了，没法生成胶水代码，这里用debug模式生成吧
+cargo build --target $target
+cargo run --bin uniffi_bindgen generate --library ./target/$target/debug/libjk_core_question.a --language swift --out-dir $outdir
+
 cargo build --target $target --release
-cargo run --bin uniffi_bindgen generate --library ./target/$target/release/libjk_core_question.a --language swift --out-dir $outdir
 cp ./target/$target/release/libjk_core_question.a $outdir
+
+# 生成的静态文件strip后会小很多
+strip ./target/$target/release/libjk_core_question.a
